71c3509ac1bba6a0346800081cc45073d9846c36
Chayan Pant <pantchayan@gmail.com>
2022-02-22 18:10:44 +0530

    update

--- datGui.js
+++ datGui.js
@@ -10,3 +10,10 @@ cameraRotationFolder.add(camera.rotation, "z", -6.28, 6.28);
 cameraRotationFolder.add(camera.rotation, "y", -6.28, 6.28);
 cameraRotationFolder.add(camera.rotation, "x", -6.28, 6.28);
 cameraFolder.open();
+
+// Camera Move
+document.addEventListener("mousewheel", (e) => {
+  camera.position.x -= e.deltaY * 0.005;
+  // prevent scrolling beyond a min/max value
+  camera.position.clampScalar(mode == 1 ? -50 : -70, 10);
+});
